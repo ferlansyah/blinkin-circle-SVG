3fe195c04975a6ba6a2833ba8097ff19ea47489a
Ferlansyah <ferlansyah@rocketmail.com>
2019-05-07 14:55:08 +0700

    Add files index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "
+<svg width=\"20\" height=\"17\">
+  <circle cx=\"10\" cy=\"9\" r=\"8\">
+    <animate attributeName=\"fill\" attributeType=\"XML\"
+    from=\"red\" to=\"blue\" dur=\"1s\" repeatCount=\"indefinite\" />
+  </circle>
+</svg>
+";
+?>
